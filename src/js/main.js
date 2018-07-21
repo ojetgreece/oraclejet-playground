1aa88a2626de520c418c807d5cfb8736b7f819bc
Calum James <calum_james@me.com>
2018-07-21 20:50:48 +0100

    Add ESLint linting and fix any errors; alter Prettier options to match ESLint config

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@
   Copyright (c) 2015, 2018, Oracle and/or its affiliates.
   The Universal Permissive License (UPL), Version 1.0
 */
-"use strict";
+
+'use strict';
 
 /**
  * Example of Require.js boostrap javascript
@@ -10,43 +11,46 @@
 /* eslint-disable quote-props */
 
 requirejs.config({
-  baseUrl: "js",
+  baseUrl: 'js',
 
   // Path mappings for the logical module names
   // Update the main-release-paths.json for release mode when updating the mappings
   paths:
+    /* eslint-disable spaced-comment */
     //injector:mainReleasePaths
     {
-      knockout: "libs/knockout/knockout-3.4.2.debug",
-      jquery: "libs/jquery/jquery-3.3.1",
-      "jqueryui-amd": "libs/jquery/jqueryui-amd-1.12.1",
-      promise: "libs/es6-promise/es6-promise",
-      hammerjs: "libs/hammer/hammer-2.0.8",
-      ojdnd: "libs/dnd-polyfill/dnd-polyfill-1.0.0",
-      ojs: "libs/oj/v5.1.0/debug",
-      ojL10n: "libs/oj/v5.1.0/ojL10n",
-      ojtranslations: "libs/oj/v5.1.0/resources",
-      text: "libs/require/text",
-      signals: "libs/js-signals/signals",
-      customElements: "libs/webcomponents/custom-elements.min",
-      proj4: "libs/proj4js/dist/proj4-src",
-      css: "libs/require-css/css"
+      knockout: 'libs/knockout/knockout-3.4.2.debug',
+      jquery: 'libs/jquery/jquery-3.3.1',
+      'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
+      promise: 'libs/es6-promise/es6-promise',
+      hammerjs: 'libs/hammer/hammer-2.0.8',
+      ojdnd: 'libs/dnd-polyfill/dnd-polyfill-1.0.0',
+      ojs: 'libs/oj/v5.1.0/debug',
+      ojL10n: 'libs/oj/v5.1.0/ojL10n',
+      ojtranslations: 'libs/oj/v5.1.0/resources',
+      text: 'libs/require/text',
+      signals: 'libs/js-signals/signals',
+      customElements: 'libs/webcomponents/custom-elements.min',
+      proj4: 'libs/proj4js/dist/proj4-src',
+      css: 'libs/require-css/css'
     },
   //endinjector
+  /* eslint-enable spaced-comment */
+
   // Shim configurations for modules that do not expose AMD
   shim: {
     jquery: {
-      exports: ["jQuery", "$"]
+      exports: ['jQuery', '$']
     }
   }
 });
 
 require([
-  "ojs/ojcore",
-  "knockout",
-  "jquery",
-  "ojs/ojknockout",
-  "ojs/ojbutton"
+  'ojs/ojcore',
+  'knockout',
+  'jquery',
+  'ojs/ojknockout',
+  'ojs/ojbutton'
 ], function(oj, ko, $) {
   $(function() {
     function init() {}
@@ -62,12 +66,12 @@ require([
         self.isPressed(true);
       };
     }
-    ko.applyBindings(new ButtonModel(), document.querySelector("main"));
+    ko.applyBindings(new ButtonModel(), document.querySelector('main'));
 
     // If running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
     // event before executing any code that might interact with Cordova APIs or plugins.
-    if ($(document.body).hasClass("oj-hybrid")) {
-      document.addEventListener("deviceready", init);
+    if ($(document.body).hasClass('oj-hybrid')) {
+      document.addEventListener('deviceready', init);
     } else {
       init();
     }
